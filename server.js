bbb6b836021afd2ee7f06649fa22e2d341966da5
aref <aref@test.com>
2024-02-03 17:51:27 +0600

    latest commit

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.use("/protected", protectedRouter);
 app.use("/admin", adminRouter);
 
 // ------- Listen ------- //
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
